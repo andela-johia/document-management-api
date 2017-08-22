47211b52a8ddaf0d71ac8701d98711a31c86b451
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-08-22 13:07:57 +0100

    create todoitems endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-  from app.models import TodoList
+  from app.models import TodoList, TodoItem
   app = FlaskAPI(__name__, instance_relative_config=True)
   app.config.from_object(app_config[config_name])
   app.config.from_pyfile('config.py')
@@ -82,6 +82,102 @@ def create_app(config_name):
         'date_modified': todolist.date_modified
       })
       response.status_code = 200
+      return response
+
+  @app.route('/api/todoitems/', methods=['POST', 'GET'])
+  def todoitem():
+    if request.method == 'POST':
+      items = str(request.data.get('items', ' '))
+      todolist_id =str(request.data.get('todolist_id', ' '))
+
+      if items:
+        todoitem = TodoItem(items=items, todolist_id=todolist_id)
+        todoitem.save()
+        response = jsonify({
+          'id': todoitem.id,
+          'items': todoitem.items,
+          'todolist_id': todoitem.todolist_id,
+          'date_created': todoitem.date_created,
+          'date_modified': todoitem.date_modified
+        })
+        response.status_code = 201
+        return response
+
+    else:
+      todoitems = TodoItem.get_all()
+      todos = []
+      for todoitem in todoitems:
+        new_obj = {
+          'id': todoitem.id,
+          'item': todoitem.items,
+          'todolist_id': todoitem.todolist_id,
+          'date_created': todoitem.date_created,
+          'date_modified': todoitem.date_modified
+        }
+        todos.append(new_obj)
+        response = jsonify(todos)
+        response.status_code = 200
+
+      return response
+
+  @app.route('/api/todoitems/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+  def todoitems_manipulation(id, **kwargs):
+    todoitem = TodoItem.query.filter_by(id=id).first()
+    if not todoitem:
+      abort(404)
+
+    if request.method == 'GET':
+      response = jsonify({
+        'id': todoitem.id,
+        'items': todoitem.items,
+        'todolist_id': todoitem.todolist_id,
+        'date_created': todoitem.date_created,
+        'date_modified': todoitem.date_modified
+      })
+      response.status_code = 200
+      return response
+
+    elif request.method == 'DELETE':
+      todolist.delete()
+      return {
+        "message": "todoitem {} deleted successfully" .format(todoitem.id)
+      }, 200
+
+    elif request.method == 'PUT':
+      name = str(request.data.get('name', ' '))
+      todolist.name = name
+      todolist.save()
+      response = jsonify({
+        'id': todoitem.id,
+        'items': todoitem.items,
+        'todolist_id': todoitem.todolist_id,
+        'date_created': todoitem.date_created,
+        'date_modified': todoitem.date_modified
+      })
+      response.status_code = 200
+      return response
+
+  @app.route('/api/todolists/<int:id>/todoitems', methods=['GET'])
+  def todolist_items_manipulation(id, **kwargs):
+    todolist = TodoList.query.filter_by(id=id).first()
+    if not todolist:
+      abort(404)
+
+    if request.method == 'GET':
+      todolist_items = TodoItem.query.filter_by(todolist_id=todolist.id)
+      todoitems = []
+
+      for todolist_item in todolist_items:
+        new_obj = {
+        'id': todolist_item.id,
+        'item': todolist_item.items,
+        'todolist_id': todolist_item.todolist_id,
+        'date_created': todolist_item.date_created,
+        'date_modified': todolist_item.date_modified
+        }
+        todoitems.append(new_obj)
+        response = jsonify(todoitems)
+        response.status_code = 200
       return response
 
   return app
--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class TodoList(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    todoitems = db.relationship('TodoItem', backref='todolist', lazy='dynamic')
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
@@ -32,4 +33,36 @@ class TodoList(db.Model):
       return "<TodoList: {}>".format(self.name)
 
 
+class TodoItem(db.Model):
+  """This class represents the Todoitems table."""
+
+  __tablename__ = 'todoitems'
+
+  id = db.Column(db.Integer, primary_key=True)
+  items = db.Column(db.Text())
+  todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'))
+  date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+  date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+  def __init__(self, items, todolist_id):
+    """initialize with name"""
+    self.items = items
+    self.todolist_id = todolist_id
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  @staticmethod
+  def get_all():
+    return TodoItem.query.all()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return "<TodoItem: {}>".format(self.name)
 
